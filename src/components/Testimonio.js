19e0bd14d73d6fe4ac8fed2d8dd75999795a149e
Denilpv08 <denilpv22gmail.com>
2022-11-12 00:52:08 -0500

    primer proyecto con react

---
+++ src/components/Testimonio.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../css/Testimonio.css';
+
+function Testimonio(props){
+  return(
+    <div className='contenedor'>
+      <img className='imagen' src={require(`../img/${props.imagen}.png`)} alt='Foto de Shawn'/>
+      <div className='texto'>
+        <p className='nombre'><b>{props.nombre}</b> en {props.pais}</p>
+        <p className='cargo'>{props.cargo} en <b>{props.empresa}</b></p>
+        <p className='descripcion'>"{props.testimonio}"</p>
+      </div>
+    </div>
+  );
+}
+
+export default Testimonio;
